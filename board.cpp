227a70bf99b2249334a133fe63741c13898ee163
SynAckPack <45155962+SynAckPack@users.noreply.github.com>
2018-11-18 20:39:31 -0700

    Add files via upload

---
+++ board.cpp
@@ -0,0 +1,29 @@
+//Casey Capp and Laura Lucas
+#include <iostream>
+#include <string>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+#include "functions.cpp"
+
+
+int main() {
+	srand(time(NULL));
+
+	carrier.shipPositionPlacement();
+	battleship.shipPositionPlacement();
+	cruiser.shipPositionPlacement();
+	submarine.shipPositionPlacement();
+	destroyer.shipPositionPlacement();
+
+	cout << "Welcome to Battleship!" << endl;
+	cout << "Enemy ship positions have been set. Fire Away!" << endl;
+
+	blankBoardDisplay();
+	shotAttempt();
+	while (!endGame == true) gameDisplay(row, col);
+
+	return 0;
+}
+
+
